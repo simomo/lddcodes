6e5ac4e9712db23fdf5c1679e668cff524cf1e1c
duozhang <duozhang@vmware.com>
2015-08-17 08:15:49 -0700

    building.... added register major, minor

--- scull-ch3/main.c
+++ scull-ch3/main.c
@@ -1,17 +1,41 @@
-#include <linux/init.h>
+/*
+ * Init and load scull
+ */
+#include <linux/init.h>  // module_init, module_exit
 #include <linux/module.h>
+#include <linux/kdev_t.h>  // MKDEV 
+#include <linux/fs.h>  // register_chrdev_region...
 
-void print_current(void);
+#include "scull.h"
 
-static int hello_init(void) {
-  printk(KERN_ALERT "Hello, world\n");
-  print_current();
-  return 0;
+
+int scull_major = SCULL_MAJOR,
+    scull_minor = SCULL_MINOR,
+    scull_nr_devs = SCULL_NR_DEVS;
+
+dev_t dev;
+int result;
+
+static int scull_init(void) {
+    printk(KERN_ALERT "scull init\n");
+    // allocate dev_t ( the device number )
+    if (scull_major) {  // static allocate 
+        dev = MKDEV(scull_major, scull_minor);
+        result = register_chrdev_region(dev, scull_nr_devs, "scull");
+    } else {
+        result = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs, "scull");
+        scull_major = MAJOR(dev);
+    }
+    // check result
+    if (result < 0) {  // fail, do unregister
+        unregister_chrdev_region(dev, scull_nr_devs);
+    }
+    return 0;
 }
 
-static void hello_exit(void) {
-  printk(KERN_ALERT "bye\n");
+static void scull_exit(void) {
+    printk(KERN_ALERT "scull end\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(scull_init);
+module_exit(scull_exit);
---
+++ scull-ch3/scull.h
@@ -0,0 +1,3 @@
+#define SCULL_MAJOR 0
+#define SCULL_MINOR 0
+#define SCULL_NR_DEVS 4
