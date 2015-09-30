8fba0d91183fbb5e84a3245febc543f57d58d7c9
duozhang <duozhang@vmware.com>
2015-09-30 01:30:08 -0700

    get dev num & generate dev file done

--- scull-ch3/main.c
+++ scull-ch3/main.c
@@ -5,6 +5,8 @@
 #include <linux/module.h>
 #include <linux/kdev_t.h>  // MKDEV 
 #include <linux/fs.h>  // register_chrdev_region...
+#include <linux/device.h>  // udev
+#include <linux/cdev.h>  // cdev
 
 #include "scull.h"
 
@@ -13,23 +15,43 @@ int scull_major = SCULL_MAJOR,
     scull_minor = SCULL_MINOR,
     scull_nr_devs = SCULL_NR_DEVS;
 
-dev_t dev;
+dev_t dev_num;
 int result;
+struct class *c_scull;
+struct cdev dev_scull;
 
 static int scull_init(void) {
     printk(KERN_ALERT "scull init\n");
     // allocate dev_t ( the device number )
     if (scull_major) {  // static allocate 
-        dev = MKDEV(scull_major, scull_minor);
-        result = register_chrdev_region(dev, scull_nr_devs, "scull");
-    } else {
-        result = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs, "scull");
-        scull_major = MAJOR(dev);
+        dev_num = MKDEV(scull_major, scull_minor);
+        result = register_chrdev_region(dev_num, scull_nr_devs, "scull");
+    } else {  // dynamic allocate
+        result = alloc_chrdev_region(&dev_num, scull_minor, scull_nr_devs, "scull");
+        scull_major = MAJOR(dev_num);
     }
     // check result
-    if (result < 0) {  // fail, do unregister
-        unregister_chrdev_region(dev, scull_nr_devs);
+    if (result < 0) {  // fail to get major
+        return -1;
     }
+
+    // automatically create device file
+    c_scull = class_create(THIS_MODULE, "scull");
+    if (c_scull == NULL) {
+        unregister_chrdev_region(dev_num, scull_nr_devs);
+        return -1;
+    }
+    dev_scull = device_create(c_scull, NULL, dev_num, NULL, "scull");
+    if (dev_scull == NULL) {
+        class_destroy(c_scull);
+        unregister_chrdev_region(dev_num, scull_nr_devs);
+        return -1;
+    }
+
+    // TODO: Implementate file operation functions
+
+
+
     return 0;
 }
 
--- scull-ch3/scull.h
+++ scull-ch3/scull.h
@@ -1,3 +1,11 @@
 #define SCULL_MAJOR 0
 #define SCULL_MINOR 0
 #define SCULL_NR_DEVS 4
+
+struct file_operations scull_file_ops = {
+    .owner = THIS_MODULE,
+    .open = scull_open,
+    .release = scull_close,
+    .read = scull_read,
+    .write = scull_write
+}
