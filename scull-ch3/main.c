a8d3f47a1d578c05b3acb4404c7578d460e971e8
duozhang <duozhang@vmware.com>
2015-10-02 08:38:03 -0700

    almost done the init and exit function

--- scull-ch3/main.c
+++ scull-ch3/main.c
@@ -15,13 +15,14 @@ int scull_major = SCULL_MAJOR,
     scull_minor = SCULL_MINOR,
     scull_nr_devs = SCULL_NR_DEVS;
 
-dev_t dev_num;
-int result;
-struct class *c_scull;
-struct cdev dev_scull;
+static dev_t dev_num;
+static int result;
+static struct class *c_scull;
+static struct cdev *dev_scull;
 
 
 // file operations functions
+static int scull_open(){}
 
 
 static int scull_init(void) {
@@ -45,22 +46,37 @@ static int scull_init(void) {
         unregister_chrdev_region(dev_num, scull_nr_devs);
         return -1;
     }
-    dev_scull = device_create(c_scull, NULL, dev_num, NULL, "scull");
+    dev_scull = device_create(c_scull, NULL, dev_num, NULL, "scull");  // device_create will do alloc_dev's work, so we can call cdev_init directly 
     if (dev_scull == NULL) {
         class_destroy(c_scull);
         unregister_chrdev_region(dev_num, scull_nr_devs);
         return -1;
     }
 
-    // TODO: Implementate file operation functions
+    // init cdev structure, then add (register it to kernel)
+    cdev_init(dev_scull, &scull_file_ops);
+    if (cdev_add(dev_scull, dev_num, scull_nr_devs) == -1) {
+        device_destroy(c_scull, dev_num);
+        class_destroy(c_scull);
+        unregister_chrdev_region(dev_num, scull_nr_devs);
+        return -1;
+    }
+
+
 
     return 0;
 }
 
 static void scull_exit(void) {
     printk(KERN_ALERT "scull end\n");
+    // reverse cdev_add, cdev_init
+    cdev_del(&dev_scull);
+    // reverse device_create
+    device_destroy(c_scull, dev_num);
+    // reverse class_create
     class_destroy(c_scull);
-    unregister_chrdev_region(dev, scull_nr_devs);
+    // reverse register_chrdev_region(static dev num) or alloc_chrdev_region(dynamic dev num)
+    unregister_chrdev_region(dev_num, scull_nr_devs);
 }
 
 module_init(scull_init);
